9685a8f384674892f5de991470d6bf7a262f7b21
koo <rnrudgh@gmail.com>
2018-09-15 11:35:18 +0900

    BUG FIX: journal write

--- block/qcow2-journal.c
+++ block/qcow2-journal.c
@@ -22,7 +22,7 @@ void qcow2_journal_write(BlockDriverState *bs, Qcow2Cache *c,void *table )
     } 
 
 
-   qcow2_cache_entry_mark_dirty( c,table);
+   //qcow2_cache_entry_mark_dirty( c,table);
 }
 
 void qcow2_journal_begin_op(BlockDriverState *bs)
